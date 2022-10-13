e11c8cc9c64661a8c65aa66432e560fd4850043f
Hao Chen <hchen56@u.rochester.edu>
2022-10-13 16:07:06 +0900

    restrict key's range

--- ciphers/shift.go
+++ ciphers/shift.go
@@ -1,17 +1,25 @@
 package shift
 
-func Encrypt(msg string, key int) string {
+import "fmt"
+
+func Encrypt(msg string, key int) (string, error) {
+	if key < 0 || key > 26 {
+		return "", fmt.Errorf("key must be between 0 and 26")
+	}
 	var encrypted string
 	for _, c := range msg {
 		encrypted += string(c + rune(key)%26)
 	}
-	return encrypted
+	return encrypted, nil
 }
 
-func Decrypt(msg string, key int) string {
+func Decrypt(msg string, key int) (string, error) {
 	var decrypted string
+	if key < 0 || key > 26 {
+		return "", fmt.Errorf("key must be between 0 and 26")
+	}
 	for _, c := range msg {
 		decrypted += string(c - rune(key)%26)
 	}
-	return decrypted
+	return decrypted, nil
 }
--- ciphers/shift_test.go
+++ ciphers/shift_test.go
@@ -1,9 +1,11 @@
 package shift
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestEncrypt(t *testing.T) {
@@ -15,21 +17,33 @@ func TestEncrypt(t *testing.T) {
 		name string
 		in   in
 		out  string
+		err  error
 	}{
 		{
 			name: "success",
 			in: in{
 				msg: "hello",
+				key: 3,
+			},
+			out: "khoor",
+			err: nil,
+		},
+		{
+			name: "key out of range",
+			in: in{
+				msg: "hello",
 				key: 27,
 			},
-			out: "ifmmp",
+			out: "",
+			err: fmt.Errorf("key must be between 0 and 26"),
 		},
 	}
 	for _, tt := range tests {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
-			out := Encrypt(tt.in.msg, tt.in.key)
+			out, err := Encrypt(tt.in.msg, tt.in.key)
+			assert.Equal(t, tt.err, err)
 			if diff := cmp.Diff(tt.out, out); diff != "" {
 				t.Errorf("encrypt() mismatch (-want +got):\n%s", diff)
 			}
@@ -46,21 +60,33 @@ func TestDecrypt(t *testing.T) {
 		name string
 		in   in
 		out  string
+		err  error
 	}{
 		{
 			name: "success",
 			in: in{
-				msg: "ifmmp",
-				key: 27,
+				msg: "khoor",
+				key: 3,
 			},
 			out: "hello",
+			err: nil,
+		},
+		{
+			name: "key out of range",
+			in: in{
+				msg: "khoor",
+				key: -1,
+			},
+			out: "",
+			err: fmt.Errorf("key must be between 0 and 26"),
 		},
 	}
 	for _, tt := range tests {
 		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
-			out := Decrypt(tt.in.msg, tt.in.key)
+			out, err := Decrypt(tt.in.msg, tt.in.key)
+			assert.Equal(t, tt.err, err)
 			if diff := cmp.Diff(tt.out, out); diff != "" {
 				t.Errorf("decrypt() mismatch (-want +got):\n%s", diff)
 			}
